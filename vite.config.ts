c7c2af9b2d0d332e92aec4b6fa4e7b9053d6c803
David <david@DESKTOP-3SAD9P2.localdomain>
2023-03-08 13:05:20 +0100

    push

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import inject from "@rollup/plugin-inject";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    global: {},
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+      os: "os-browserify/browser",
+      util: "util",
+      process: "process/browser",
+      buffer: "buffer",
+    },
+  },
+});
